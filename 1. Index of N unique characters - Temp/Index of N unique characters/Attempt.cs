e805e965319661eee188653a8f5354c3164c3ab2
Gabriel Dege <gdege23@students.psc.ac.uk>
2024-09-25 12:27:33 +0100

    temp as github doesn't work on my laptop

---
+++ 1. Index of N unique characters - Temp/Index of N unique characters/Attempt.cs
@@ -0,0 +1,26 @@
+﻿namespace Index_of_N_unique_characters
+{
+    internal class Attempt
+    {
+        /*  
+            You can ask me for an explanation if this doesn't make sense.
+
+            There us a stream (long string) of random lowercase characters.
+            You are given a value called n.
+            Return the index (of the stream) of the first occurrence where there is no repetition within n characters.
+            If there is no occurrence of n unique characters, then return -1.
+            
+            Examples: 
+                for stream = "abababc" and n = 3, the index would be 4.
+                                  ^ ^ -> all unique characters
+                for stream = "mjqjpqmgbljsphdztnvjfqwrcgsmlb" and n = 14, the index would be 5.
+                                   ^            ^ -> all unique characters     
+
+            You can alter anything within this class bar ReturnIndex's name
+        */
+        public int ReturnIndex(string stream, int n)
+        {
+            return -1;
+        }
+    }
+}
